52960c1e8e03c96c6997a85e52e43de4fe3feb71
Andreea Tataru <andreeatataru13@gmail.com>
2023-02-09 19:44:17 +0200

    prepare form to submit/add new team

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-fetch("teams.json")
-  .then((r) => r.json())
+fetch("teams.json") //aduce date
+  .then((r) => r.json()) //transforma
   .then((teams) => {
+    //console.info("r1", teams);
     displayTeams(teams);
   });
 
 function displayTeams(teams) {
+  console.info("r1", teams);
   const teamsHTML = teams.map(
+    //primesc un json si il transform in string
     (team) => ` 
         <tr>
             <td>${team.promotion}</td>
             <td>${team.members}</td>
             <td>${team.name}</td>
             <td>${team.url}</td>
+            <td></td>
         </tr>`
   );
+  console.info("teamsHTML", teamsHTML);
 
   document.querySelector("#teams tbody").innerHTML = teamsHTML.join("");
-  console.info("r1", teamsHTML, teams);
 }
+
+//din name vin campurile in network
